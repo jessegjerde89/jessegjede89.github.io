5aa9722a97f5848fefc220a628265f6a53fa3494
Jesse G <46469186+jessegjerde89@users.noreply.github.com>
2020-07-22 13:34:34 -0600

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -83,7 +83,7 @@ class Contact extends Component {
 						   {name}<br />
 						   {street} <br />
 						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
+						   
 					   </p>
 				   </div>
             </aside>
