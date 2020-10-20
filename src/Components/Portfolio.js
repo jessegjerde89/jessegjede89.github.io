dae9e0345c37ef6ca92b1a12deaad64c4907bec1
Jesse Gjerde <jesseg@MacBook-Pro.local>
2020-10-20 10:43:17 -0600

    layout update

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,13 +11,11 @@ class Portfolio extends Component {
               <a href={projects.url} title={projects.title}>
                 <img alt={projects.title} src={projectImage} />
                 <div className="overlay">
+                  {projects.title}
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
                     <p>{projects.category}</p>
                   </div>
-                </div>
-                <div className="link-icon">
-                  <i className="fa fa-link"></i>
                 </div>
               </a>
             </div>
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -30,17 +30,6 @@ class Resume extends Component {
   }
 
   render() {
-    // if(this.props.data){
-    //   var education = this.props.data.education.map(function(education){
-    //     return <div key={education.school}><h3>{education.school}</h3>
-    //     <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-    //     <p className="info-des">{education.description}</p></div>
-    //   })
-    //   var skills = this.props.data.skills.map(function(skills){
-    //     var className = 'bar-expand '+skills.name.toLowerCase();
-    //     return <li id="multipl" key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-    //   })
-    // }
     return (
       <div>
         <section id="resume">
@@ -49,19 +38,6 @@ class Resume extends Component {
               <Resume_page />
             </div>
             <div />
-
-            {/* <div className="row education">
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 <div className ="test-colour">
-                 {education}
-                 </div>
-               </div>
-            </div>
-         </div>
-      </div> */}
 
             <div className="icons">
               <div>
